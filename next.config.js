3a2acefe64d35aec5471afacd0433b4724cb597d
Eron Salling <esalling23@gmail.com>
2025-07-22 11:00:42 -0400

    Fix for nextjs deploy & GHP workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  basePath: '/ModuleTemplate',
+  assetPrefix: '/ModuleTemplate/',
   output: 'export',
+  images: {
+    unoptimized: true, // Required for static export
+  }
 };
 
 export default nextConfig;
